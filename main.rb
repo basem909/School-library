76f4e3ff1eeec34aa201aba57200f6bc66a9f375
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-07-27 20:42:35 -0300

    Fix linters errors

--- main.rb
+++ main.rb
@@ -3,10 +3,10 @@ require_relative('./trimmer')
 require_relative('./capitalize')
 require_relative('./nameable')
 require_relative('./decorator')
-#p person = Person.new(22, 'maximilianus')
-#p person.correct_name
-#capitalizedPerson = Capitalize.new(person)
-#p capitalizedPerson
-#p capitalizedPerson.correct_name
-#capitalizedTrimmedPerson = Trimmer.new(capitalizedPerson)
-#p capitalizedTrimmedPerson.correct_name
+p person = Person.new(22, 'maximilianus')
+p person.correct_name
+# capitalizedPerson = Capitalize.new(person)
+# p capitalizedPerson
+# p capitalizedPerson.correct_name
+# capitalizedTrimmedPerson = Trimmer.new(capitalizedPerson)
+# p capitalizedTrimmedPerson.correct_name
