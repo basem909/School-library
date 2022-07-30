dad7a405bfba32bcf0f31d5c5255a6191c199b11
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-07-30 06:49:57 -0300

    Update main.rb file

--- main.rb
+++ main.rb
@@ -1,12 +1,61 @@
-require_relative('./person')
-require_relative('./trimmer')
-require_relative('./capitalize')
-require_relative('./nameable')
-require_relative('./decorator')
-p person = Person.new(22, 'maximilianus')
-p person.correct_name
-capitalizedPerson = Capitalize.new(person)
- p capitalizedPerson
-p capitalizedPerson.correct_name
-# capitalizedTrimmedPerson = Trimmer.new(capitalizedPerson)
-# p capitalizedTrimmedPerson.correct_name
+require './app'
+
+class Main
+  attr_reader :app
+
+  def initialize
+    puts 'School library application'
+    @app = App.new
+    item_list
+  end
+
+  def item_list
+    puts '================================'
+    puts 'Choose an option:'
+    puts '================================'
+    puts "
+  1 - List all books
+  2 - List all people
+  3 - Create a person
+  4 - Create a book
+  5 - Create a rental
+  6 - List all rentals for a given person id
+  7 - Exit"
+    selection_user
+  end
+
+  # rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/MethodLength
+  def selection_user
+    selection = gets.chomp.to_i
+    case selection
+    when 1
+      app.books_list
+      item_list
+    when 2
+      app.people_list
+      item_list
+    when 3
+      app.add_person
+      item_list
+    when 4
+      app.add_book
+      item_list
+    when 5
+      app.rent_book
+      item_list
+    when 6
+      app.list_rentals
+      item_list
+    when 7
+      abort('Thank you for using this app!')
+    else
+      puts 'Invalid selection'
+      item_list
+    end
+  end
+  # rubocop:enable Metrics/CyclomaticComplexity
+  # rubocop:enable Metrics/MethodLength
+end
+
+Main.new
